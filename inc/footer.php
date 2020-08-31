eb90dcdadaf5807fa12e5f27a33534b6f9d81118
benybodipo <benybodipo@gmail.com>
2020-08-31 14:26:02 +0200

    First commit

---
+++ inc/footer.php
@@ -0,0 +1,12 @@
+</div>
+</main>
+<footer>
+<div class="container">
+	<p>Copyright © 2018 Camagru</p><br>
+	<p>Web Master - Benito E Bodipo</p>
+</div>
+</footer>
+	<script src="resources/js/master.js" charset="utf-8"></script>
+	<?php  includeJs($pageName, $jsList);?>
+</body>
+</html>
